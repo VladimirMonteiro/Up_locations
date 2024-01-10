412e88d19003ed6c537021b29f6b2e1314c2a092
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-01-10 14:07:06 -0300

    a

--- src/components/card_product/CardProduct.js
+++ src/components/card_product/CardProduct.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 const CardProduct = ({image ,name, description, alt, url}) => {
     return (
         <div className='product-single'>
-                <img src={image} alt = {alt}></img>
+                <img crossOrigin='anonymous' src={image} alt = {alt}></img>
                 <h3>{name}</h3>
                 <p>{description.substring(0,30)}...</p>
                 <Link style={{color: 'white'}}to={url}>Alugar</Link>
